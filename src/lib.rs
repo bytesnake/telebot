055c60c7157043441fe7e541a7c54d93db8c3e7a
l_schmid <l_schmid@cojobo.net>
2017-01-18 12:27:54 +0100

    correct the identation of the example

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 //! # Write a telegram bot in Rust
 //!
 //! This library allows you to write a Telegram Bot in Rust. It's an almost complete wrapper for the Telegram Bot API and uses tokio-curl to send a request to the Telegram server. Each Telegram function call returns a future and carries the actual bot and the answer.
-//! You can find all available functions in src/functions.rs. The getter/setter etc. will be
-//! automatically implemented by telebot-derive. 
+//! You can find all available functions in src/functions.rs. The crate telebot-derive implements all
+//! required getter, setter and send functions automatically.
 //!
 //! # Example usage
 //!
@@ -19,21 +19,33 @@
 //! 
 //! // import all available functions
 //! use telebot::functions::*;
+//! 
 //! fn main() {
+//!     // create a new event loop
 //!     let mut lp = Core::new().unwrap();
+//!     
+//!     // init the bot with the bot key and an update interval of 200ms
 //!     let bot = bot::RcBot::new(lp.handle(), "<TELEGRAM-BOT-TOKEN>")
 //!         .update_interval(200);
+//!
+//!     // register a new command "reply" which replies all received messages
 //!     let handle = bot.new_cmd("/reply")
 //!     .and_then(|(bot, msg)| {
 //!         let mut text = msg.text.unwrap().clone();
+//!
+//!         // when the text is empty send a dummy text
 //!         if text.is_empty() {
 //!             text = "<empty>".into();
 //!         }
 //!
+//!         // construct a message and return a new future which will be resolved by tokio
 //!         bot.message(msg.chat.id, text).send()
 //!     });
+//!
+//!     // register the new command
 //!     bot.register(handle);
 //!
+//!     // start the event loop
 //!     bot.run(&mut lp).unwrap();
 //! }
 //! ```
