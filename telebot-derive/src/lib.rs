0f51d61a7baafe06b73d056da2f112f70d5b9a1b
Ayrat Badykov <ayratin555@gmail.com>
2020-04-12 16:50:00 +0300

    fix indentation

--- telebot-derive/src/lib.rs
+++ telebot-derive/src/lib.rs
@@ -1,79 +1,94 @@
-    #![recursion_limit="150"]
+#![recursion_limit = "150"]
 
-    extern crate log;
-    extern crate proc_macro;
-    #[macro_use]
-    extern crate quote;
-    extern crate syn;
+extern crate log;
+extern crate proc_macro;
+#[macro_use]
+extern crate quote;
+extern crate syn;
 
-    use proc_macro::TokenStream;
-    use std::collections::BTreeMap;
+use proc_macro::TokenStream;
+use std::collections::BTreeMap;
 
-    #[proc_macro_derive(setter, attributes(query))]
-    pub fn derive_setter(input: TokenStream) -> TokenStream {
-        let ast = syn::parse_macro_input(&input.to_string()).unwrap();
-        let expanded = expand_setter(ast);
-        expanded.to_string().parse().unwrap()
-    }
+#[proc_macro_derive(setter, attributes(query))]
+pub fn derive_setter(input: TokenStream) -> TokenStream {
+    let ast = syn::parse_macro_input(&input.to_string()).unwrap();
+    let expanded = expand_setter(ast);
+    expanded.to_string().parse().unwrap()
+}
 
-    fn expand_setter(ast: syn::MacroInput) -> quote::Tokens {
-        let config = config_from(&ast.attrs);
+fn expand_setter(ast: syn::MacroInput) -> quote::Tokens {
+    let config = config_from(&ast.attrs);
 
-        let query_kind = config.get("query").map(|tmp| syn::Lit::from(tmp.as_str()));
-        //let file_kind = config.get("file_kind").map(|tmp| syn::Ident::from(tmp.as_str()));
+    let query_kind = config.get("query").map(|tmp| syn::Lit::from(tmp.as_str()));
+    //let file_kind = config.get("file_kind").map(|tmp| syn::Ident::from(tmp.as_str()));
 
-        let fields: Vec<_> = match ast.body {
-            syn::Body::Struct(syn::VariantData::Struct(ref fields)) => {
-                fields.iter().map(|f| (f.ident.as_ref().unwrap(), &f.ty)).collect()
+    let fields: Vec<_> = match ast.body {
+        syn::Body::Struct(syn::VariantData::Struct(ref fields)) => fields
+            .iter()
+            .map(|f| (f.ident.as_ref().unwrap(), &f.ty))
+            .collect(),
+        syn::Body::Struct(syn::VariantData::Unit) => vec![],
+        _ => panic!("#[derive(getters)] can only be used with braced structs"),
+    };
+
+    let name = &ast.ident;
+    let is_option_ident = |ref f: &(&syn::Ident, &syn::Ty)| -> bool {
+        match *f.1 {
+            syn::Ty::Path(_, ref path) => match path.segments.first().unwrap().ident.as_ref() {
+                "Option" => true,
+                _ => false,
             },
-            syn::Body::Struct(syn::VariantData::Unit) => {
-                vec![]
+            _ => false,
+        }
+    };
+
+    let field_compulsory: Vec<_> = fields
+        .iter()
+        .filter(|f| !is_option_ident(&f))
+        .filter(|f| f.0.as_ref() != "kind" && f.0.as_ref() != "id")
+        .map(|f| syn::Ident::from(format!("_{}", f.0.as_ref())))
+        .collect();
+
+    let field_optional: Vec<_> = fields
+        .iter()
+        .filter(|f| is_option_ident(&f))
+        .map(|f| f.0)
+        .collect();
+    let field_optional2 = field_optional.clone();
+
+    let field_compulsory2: Vec<_> = fields
+        .iter()
+        .map(|f| f.0)
+        .filter(|f| f.as_ref() != "kind" && f.as_ref() != "id")
+        .collect();
+
+    let field_compulsory3 = field_compulsory.clone();
+    let values: Vec<_> = fields
+        .iter()
+        .filter(|f| f.0.as_ref() != "kind" && f.0.as_ref() != "id")
+        .map(|f| match *f.1 {
+            syn::Ty::Path(_, ref path) => match path.segments.first().unwrap().ident.as_ref() {
+                "Option" => return syn::Ident::from("None"),
+                _ => return syn::Ident::from(format!("_{}", f.0.as_ref())),
             },
-            _ => panic!("#[derive(getters)] can only be used with braced structs"),
-        };
+            _ => return syn::Ident::from("None"),
+        })
+        .collect();
 
-        let name = &ast.ident;
-        let is_option_ident = |ref f: &(&syn::Ident, &syn::Ty)| -> bool {
-            match *f.1 {
-                syn::Ty::Path(_, ref path) => {
-                    match path.segments.first().unwrap().ident.as_ref() {
-                        "Option" => true,
-                        _ => false
-                    }
-                },
-                _ => false
-            }
-        };
-
-        let field_compulsory: Vec<_> = fields.iter().filter(|f| !is_option_ident(&f))
-            .filter(|f| f.0.as_ref() != "kind" && f.0.as_ref() != "id")
-            .map(|f| syn::Ident::from(format!("_{}", f.0.as_ref())))
-            .collect();
-
-        let field_optional: Vec<_> = fields.iter().filter(|f| is_option_ident(&f)).map(|f| f.0).collect();
-        let field_optional2 = field_optional.clone();
-
-        let field_compulsory2: Vec<_> = fields.iter().map(|f| f.0).filter(|f| f.as_ref() != "kind" && f.as_ref() != "id").collect();
-
-
-        let field_compulsory3 = field_compulsory.clone();
-        let values: Vec<_> = fields.iter().filter(|f| f.0.as_ref() != "kind" && f.0.as_ref() != "id").map(|f| {
-            match *f.1 {
-                syn::Ty::Path(_, ref path) => {
-                    match path.segments.first().unwrap().ident.as_ref() {
-                        "Option" => return syn::Ident::from("None"),
-                        _ => return syn::Ident::from(format!("_{}", f.0.as_ref()))
-                    }
-                },
-                _ => return syn::Ident::from("None")
-            }
-        }).collect();
-
-        //let ty_compulsory: Vec<_> = fields.iter().map(|f| f.1).collect();
-        let ty_compulsory2: Vec<_> = fields.iter().filter(|f| f.0.as_ref() != "kind" && f.0.as_ref() != "id").map(|f| f.1).collect();
-        let ty_optional: Vec<_> = fields.iter().filter(|f| is_option_ident(&f)).map(|f| {
+    //let ty_compulsory: Vec<_> = fields.iter().map(|f| f.1).collect();
+    let ty_compulsory2: Vec<_> = fields
+        .iter()
+        .filter(|f| f.0.as_ref() != "kind" && f.0.as_ref() != "id")
+        .map(|f| f.1)
+        .collect();
+    let ty_optional: Vec<_> = fields
+        .iter()
+        .filter(|f| is_option_ident(&f))
+        .map(|f| {
             if let syn::Ty::Path(_, ref path) = *f.1 {
-                if let syn::PathParameters::AngleBracketed(ref param) = path.segments.first().unwrap().parameters {
+                if let syn::PathParameters::AngleBracketed(ref param) =
+                    path.segments.first().unwrap().parameters
+                {
                     if let &syn::Ty::Path(_, ref path) = param.types.first().unwrap() {
                         return (*path).clone();
                     }
@@ -81,58 +96,59 @@
             }
 
             panic!("no sane type!");
-        }).collect();
+        })
+        .collect();
 
-        //println!("{:?}", ty_optional.first());
+    //println!("{:?}", ty_optional.first());
 
-        //let trait_name = syn::Ident::from(format!("Function{}",  name.as_ref()));
-        //let wrapper_name = syn::Ident::from(format!("Wrapper{}", name.as_ref()));
+    //let trait_name = syn::Ident::from(format!("Function{}",  name.as_ref()));
+    //let wrapper_name = syn::Ident::from(format!("Wrapper{}", name.as_ref()));
 
-        if let Some(query_name) = query_kind {
-            quote! {
-                impl #name {
-                    #[allow(dead_code)]
-                    pub fn new(#( #field_compulsory3: #ty_compulsory2, )*) -> #name {
-                        let id = Uuid::new_v4();
+    if let Some(query_name) = query_kind {
+        quote! {
+            impl #name {
+                #[allow(dead_code)]
+                pub fn new(#( #field_compulsory3: #ty_compulsory2, )*) -> #name {
+                    let id = Uuid::new_v4();
 
-                        #name { kind: #query_name.into(), id: id.to_hyphenated().to_string(), #( #field_compulsory2: #values, )* }
-                    }
-                    #(
-                        pub fn #field_optional<S>(mut self, val: S) -> Self where S: Into<#ty_optional> {
-                            self.#field_optional2 = Some(val.into());
-
-                            self
-                        }
-                    )*
+                    #name { kind: #query_name.into(), id: id.to_hyphenated().to_string(), #( #field_compulsory2: #values, )* }
                 }
+                #(
+                    pub fn #field_optional<S>(mut self, val: S) -> Self where S: Into<#ty_optional> {
+                        self.#field_optional2 = Some(val.into());
 
-            }
-        } else {
-            quote! {
-                impl #name {
-                    #[allow(dead_code)]
-                    pub fn new(#( #field_compulsory3: #ty_compulsory2, )*) -> #name {
-                        #name { #( #field_compulsory2: #values, )* }
+                        self
                     }
-                    #(
-                        pub fn #field_optional<S>(mut self, val: S) -> Self where S: Into<#ty_optional> {
-                            self.#field_optional2 = Some(val.into());
-
-                            self
-                        }
-                    )*
-                }
-
+                )*
             }
+
+        }
+    } else {
+        quote! {
+            impl #name {
+                #[allow(dead_code)]
+                pub fn new(#( #field_compulsory3: #ty_compulsory2, )*) -> #name {
+                    #name { #( #field_compulsory2: #values, )* }
+                }
+                #(
+                    pub fn #field_optional<S>(mut self, val: S) -> Self where S: Into<#ty_optional> {
+                        self.#field_optional2 = Some(val.into());
+
+                        self
+                    }
+                )*
+            }
+
         }
     }
+}
 
-    #[proc_macro_derive(TelegramFunction, attributes(call, answer, function, file_kind))]
-    pub fn derive_telegram_sendable(input: TokenStream) -> TokenStream {
-        let ast = syn::parse_macro_input(&input.to_string()).unwrap();
-        let expanded = expand_function(ast);
-        expanded.to_string().parse().unwrap()
-    }
+#[proc_macro_derive(TelegramFunction, attributes(call, answer, function, file_kind))]
+pub fn derive_telegram_sendable(input: TokenStream) -> TokenStream {
+    let ast = syn::parse_macro_input(&input.to_string()).unwrap();
+    let expanded = expand_function(ast);
+    expanded.to_string().parse().unwrap()
+}
 
 fn expand_function(ast: syn::MacroInput) -> quote::Tokens {
     let config = config_from(&ast.attrs);
@@ -141,73 +157,83 @@ fn expand_function(ast: syn::MacroInput) -> quote::Tokens {
     let function = syn::Lit::Str((*function).clone(), syn::StrStyle::Cooked);
     let bot_function = syn::Ident::from(config.get("function").unwrap().as_str());
     let answer = syn::Ident::from(config.get("answer").unwrap().as_str());
-    let file_kind = config.get("file_kind").map(|tmp| syn::Ident::from(tmp.as_str()));
+    let file_kind = config
+        .get("file_kind")
+        .map(|tmp| syn::Ident::from(tmp.as_str()));
 
     let fields: Vec<_> = match ast.body {
-        syn::Body::Struct(syn::VariantData::Struct(ref fields)) => {
-            fields.iter().map(|f| (f.ident.as_ref().unwrap(), &f.ty)).collect()
-        },
-        syn::Body::Struct(syn::VariantData::Unit) => {
-            vec![]
-        },
+        syn::Body::Struct(syn::VariantData::Struct(ref fields)) => fields
+            .iter()
+            .map(|f| (f.ident.as_ref().unwrap(), &f.ty))
+            .collect(),
+        syn::Body::Struct(syn::VariantData::Unit) => vec![],
         _ => panic!("#[derive(getters)] can only be used with braced structs"),
     };
 
-
-        /*for field in &fields {
+    /*for field in &fields {
         println!("{:?}", field.1);
     }*/
 
     let name = &ast.ident;
     let is_option_ident = |ref f: &(&syn::Ident, &syn::Ty)| -> bool {
         match *f.1 {
-            syn::Ty::Path(_, ref path) => {
-                match path.segments.first().unwrap().ident.as_ref() {
-                    "Option" => true,
-                    _ => false
-                }
+            syn::Ty::Path(_, ref path) => match path.segments.first().unwrap().ident.as_ref() {
+                "Option" => true,
+                _ => false,
             },
-            _ => false
+            _ => false,
         }
     };
 
-    let field_compulsory: Vec<_> = fields.iter().filter(|f| !is_option_ident(&f))
-        .map(|f| syn::Ident::from(format!("_{}", f.0.as_ref()))).collect();
+    let field_compulsory: Vec<_> = fields
+        .iter()
+        .filter(|f| !is_option_ident(&f))
+        .map(|f| syn::Ident::from(format!("_{}", f.0.as_ref())))
+        .collect();
 
-    let field_optional: Vec<_> = fields.iter().filter(|f| is_option_ident(&f)).map(|f| f.0).collect();
+    let field_optional: Vec<_> = fields
+        .iter()
+        .filter(|f| is_option_ident(&f))
+        .map(|f| f.0)
+        .collect();
     let field_optional2 = field_optional.clone();
 
     let field_compulsory2: Vec<_> = fields.iter().map(|f| f.0).collect();
     let field_compulsory3 = field_compulsory.clone();
-    let values: Vec<_> = fields.iter().map(|f| {
-        match *f.1 {
-            syn::Ty::Path(_, ref path) => {
-                match path.segments.first().unwrap().ident.as_ref() {
-                    "Option" => return syn::Ident::from("None"),
-                    _ => return syn::Ident::from(format!("_{}", f.0.as_ref()))
-                }
+    let values: Vec<_> = fields
+        .iter()
+        .map(|f| match *f.1 {
+            syn::Ty::Path(_, ref path) => match path.segments.first().unwrap().ident.as_ref() {
+                "Option" => return syn::Ident::from("None"),
+                _ => return syn::Ident::from(format!("_{}", f.0.as_ref())),
             },
-            _ => return syn::Ident::from("None")
-        }
-    }).collect();
+            _ => return syn::Ident::from("None"),
+        })
+        .collect();
 
     let ty_compulsory: Vec<_> = fields.iter().map(|f| f.1).collect();
     let ty_compulsory2 = ty_compulsory.clone();
-    let ty_optional: Vec<_> = fields.iter().filter(|f| is_option_ident(&f)).map(|f| {
-        if let syn::Ty::Path(_, ref path) = *f.1 {
-            if let syn::PathParameters::AngleBracketed(ref param) = path.segments.first().unwrap().parameters {
-                if let &syn::Ty::Path(_, ref path) = param.types.first().unwrap() {
-                    return (*path).clone();
+    let ty_optional: Vec<_> = fields
+        .iter()
+        .filter(|f| is_option_ident(&f))
+        .map(|f| {
+            if let syn::Ty::Path(_, ref path) = *f.1 {
+                if let syn::PathParameters::AngleBracketed(ref param) =
+                    path.segments.first().unwrap().parameters
+                {
+                    if let &syn::Ty::Path(_, ref path) = param.types.first().unwrap() {
+                        return (*path).clone();
+                    }
                 }
             }
-        }
 
-        panic!("no sane type!");
-    }).collect();
+            panic!("no sane type!");
+        })
+        .collect();
 
     //println!("{:?}", ty_optional.first());
 
-    let trait_name = syn::Ident::from(format!("Function{}",  name.as_ref()));
+    let trait_name = syn::Ident::from(format!("Function{}", name.as_ref()));
     let wrapper_name = syn::Ident::from(format!("Wrapper{}", name.as_ref()));
 
     let tokens = quote! {
@@ -361,11 +387,10 @@ fn config_from(attrs: &[syn::Attribute]) -> BTreeMap<String, String> {
             let name = format!("{}", name);
             let value = match value.clone() {
                 syn::Lit::Str(value, _) => value,
-                _ => panic!("bla")
+                _ => panic!("bla"),
             };
             result.insert(name, value);
         }
     }
     result
 }
-
